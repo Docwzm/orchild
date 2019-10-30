80419ca15d5be3ae741ba7b5321c5ff55efd2edf
qinmenghuan <qinmenghuan@ovfintech.com>
2019-10-30 09:51:47 +0800

    eslint

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -86,45 +86,45 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
-const F2 = require("@antv/f2");
+import { Component, Prop, Vue } from 'vue-property-decorator'
+const F2 = require('@antv/f2')
 
 @Component
 export default class HelloWorld extends Vue {
     @Prop() private msg!: string;
-    private mounted() {
-        console.log("mounted");
-        console.log("created");
+    private mounted () {
+        console.log('mounted')
+        console.log('created')
         // F2 对数据源格式的要求，仅仅是 JSON 数组，数组的每个元素是一个标准 JSON 对象。
         const data = [
-            { genre: "Sports", sold: 275 },
-            { genre: "Strategy", sold: 115 },
-            { genre: "Action", sold: 120 },
-            { genre: "Shooter", sold: 350 },
-            { genre: "Other", sold: 150 }
-        ];
+            { genre: 'Sports', sold: 275 },
+            { genre: 'Strategy', sold: 115 },
+            { genre: 'Action', sold: 120 },
+            { genre: 'Shooter', sold: 350 },
+            { genre: 'Other', sold: 150 }
+        ]
 
         // Step 1: 创建 Chart 对象
         const chart = new F2.Chart({
-            id: "mountNode",
+            id: 'mountNode',
             pixelRatio: window.devicePixelRatio // 指定分辨率
-        });
+        })
 
         // Step 2: 载入数据源
-        chart.source(data);
+        chart.source(data)
 
         // Step 3：创建图形语法，绘制柱状图，由 genre 和 sold 两个属性决定图形位置，genre 映射至 x 轴，sold 映射至 y 轴
         chart
             .interval()
-            .position("genre*sold")
-            .color("genre");
+            .position('genre*sold')
+            .color('genre')
 
         // Step 4: 渲染图表
-        chart.render();
+        chart.render()
     }
 
-    private created() {
-        
+    private created () {
+
     }
 }
 </script>
