a3c68d2b3782435365a62c671689bddf6b3ee6dc
huquanxing <huquanxing@ovfintech.com>
2019-11-12 16:30:51 +0800

    功能完善

--- src/api/home.server.ts
+++ src/api/home.server.ts
@@ -1,7 +1,7 @@
 import request from '@/utils/request'
 
 class HomeService {
-    static productList (data: any) {
+    static productList(data: any) {
         return request({
             method: 'post',
             url: '/orchid-web-customer/product/anloginfile/list/byarea/v1',
@@ -12,7 +12,7 @@ class HomeService {
      * 获取当前申请信息 流水号
      * @param data
      */
-    static getApplyInfo (data: any) {
+    static getApplyInfo(data: any) {
         return request({
             method: 'post',
             url: '/orchid-web-customer/credit/apply/v1',
@@ -23,10 +23,43 @@ class HomeService {
      * 申请融资
      * @param data
      */
-    static applyFinancing (data: any) {
+    static applyFinancing(data: any) {
         return request({
             method: 'post',
             url: '/orchid-web-customer/credit/submit/v1',
+            data
+        })
+    }
+    /**
+     * 获取附件类型
+     * @param data
+     */
+    static getAttachList(data: any) {
+        return request({
+            method: 'post',
+            url: '/orchid-unify/attach/findSubType/v1',
+            data
+        })
+    }
+    /**
+     * 获取附件类型
+     * @param data
+     */
+    static uploadFile(data: any) {
+        return request({
+            method: 'post',
+            url: '/orchid-unify/attach/upload/v1',
+            data
+        })
+    }
+    /**
+     * 获取附件类型
+     * @param data
+     */
+    static deleteFiles(data: any) {
+        return request({
+            method: 'post',
+            url: '/orchid-unify/attach/delete/v1',
             data
         })
     }
--- src/views/Client/Enclosure/Enclosure.vue
+++ src/views/Client/Enclosure/Enclosure.vue
@@ -1,13 +1,19 @@
 <template>
     <div class="enclosure">
-        <section class="van-doc-demo-block">
-            <h2 class="van-doc-demo-block__title">身份证正面</h2>
-            <van-uploader v-model="fileList" multiple/>
+        <section class="van-doc-demo-block" v-for="item in fileList" :key="item.id" :title="item">
+            <h2 class="van-doc-demo-block__title">{{item.value}}</h2>
+            <van-uploader
+                v-model="item.attachmentList"
+                :after-read="afterRead"
+                :name="item.code"
+                @delete="deleteImg"
+                multiple
+            />
         </section>
-        <section class="van-doc-demo-block">
+        <!-- <section class="van-doc-demo-block">
             <h2 class="van-doc-demo-block__title">身份证反面</h2>
             <van-uploader v-model="fileList" multiple deletable/>
-        </section>
+        </section>-->
     </div>
 </template>
 <style lang="scss" src="./Enclosure.scss"></style>
