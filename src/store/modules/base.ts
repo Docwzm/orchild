ee426bb365df43f227ce39707248c15cf8f823a0
qinmenghuan <qinmenghuan@ovfintech.com>
2019-11-14 10:00:38 +0800

    user store

--- src/store/modules/base.ts
+++ src/store/modules/base.ts
@@ -8,9 +8,9 @@ var commit: any
 
 const base = {
     state: {
-        loginUserInfo: [], // 当前登录用户
-        loginUserOrganizations: [], // 当前登录用户所处所有
-        loginUserCurrentOrganization: {}, // 当前登录用户
+        loginUserInfo: [], // 当前登录用户对象
+        loginUserOrganizations: [], // 当前登录用户关联所有机构数组，可能包括个人对象
+        loginUserCurrentOrganization: {}, // 当前登录用户的默认机构
         selectProductId: '', // 选中的金融产品id
         dictionaryData: [], // 数据字典数组
         tabBarActiveIndex: 0,//tabbar索引
