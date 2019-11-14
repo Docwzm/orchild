aa074ea4af5c047a199580b277b40d5dde9583ea
guanyj <guanyunjie@ovfintech.com>
2019-11-14 20:05:45 +0800

    数字证书

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,8 @@ module.exports = {
         'semi': 0,
         'space-before-function-paren': 0,
         'quotes': 0,
-        'object-curly-spacing': 0
+        'object-curly-spacing': 0,
+        'comma-dangle': 0
     },
     parserOptions: {
         parser: '@typescript-eslint/parser'
